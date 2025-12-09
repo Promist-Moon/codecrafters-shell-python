feb6ffb44766d62ae4c5f5089594f5d7c6952cc7
Emilia Ayu <emiliaayu944@gmail.com>
2025-12-10 01:39:31 +0800

    Fix locate executable files

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 
 
@@ -8,20 +9,23 @@ def main():
 
         commands = ["echo", "type", "exit"]
         
-        command = input()
+        ipt = input()
+        parts = ipt.split(maxsplit=1)
+        command = parts[0]
+        args = parts[1] if len(parts) > 1 else ""
+
         if command == "exit":
             sys.exit(0)
-        elif command.startswith("echo"):
+        elif command == "echo":
             print(command[5:])
-        elif command.startswith("type"):
-            cmd = command[5:]
-            if cmd in commands:
-                print(f"{cmd} is a shell builtin")
-            elif os.access(cmd, os.X_OK):
+        elif command == "type":
+            if args in commands:
+                print(f"{args} is a shell builtin")
+            elif shutil.which(args):
                 # get absolute path
-                print(f"{cmd} is {os.path.abspath(cmd)}")
+                print(f"{args} is {os.path.abspath(args)}")
             else:
-                print(f"{cmd}: not found")
+                print(f"{args}: not found")
         else:
             print(f"{command}: command not found")
         pass

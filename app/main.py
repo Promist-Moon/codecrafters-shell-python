c2bf83bba1bd3a6af66e69c2fd8d3e9c1e9bc7fb
Emilia Ayu <emiliaayu944@gmail.com>
2025-12-10 01:23:36 +0800

    Implement type

--- app/main.py
+++ app/main.py
@@ -5,12 +5,20 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
+
+        commands = ["echo", "type", "exit"]
         
         command = input()
         if command == "exit":
             sys.exit(0)
         elif command.startswith("echo"):
             print(command[5:])
+        elif command.startswith("type"):
+            cmd = command[5:]
+            if cmd in commands:
+                print(f"{cmd} is a shell builtin")
+            else:
+                print(f"{cmd}: not found")
         else:
             print(f"{command}: command not found")
         pass
